58d6c5f063f7694c08bbcfaf121b05a98097f184
shadowk97 <shadowk97@gamil.com>
2021-02-21 17:27:44 +0530

    test deploy

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-
-import './App.css';
+import './App.css'
 import Home from './components/home'
 import Navbarr from './components/navbar'
 import Footer from './components/footer'
 import LightboxPage from './components/oldPhotos'
 import TemplePhotos from './components/templephotos'
 import Trusties from './components/trusties'
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      
-      <Navbarr/>
-      <Home />
+    <div className='App'>
+      <Navbarr />
 
       <BrowserRouter>
-        <Route path="/home" component ={Home} />
-        <Route path = "/oldphotos" component= {LightboxPage} />
-        <Route path = "/templephotos" component= {TemplePhotos} />
-        <Route path = "/trusties" component= {Trusties} />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/oldphotos' component={LightboxPage} />
+          <Route exact path='/templephotos' component={TemplePhotos} />
+          <Route exact path='/trusties' component={Trusties} />
+          <Redirect to='/' />
+        </Switch>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
